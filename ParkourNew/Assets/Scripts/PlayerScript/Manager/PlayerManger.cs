b6a7f7162bc050ed1f307d795b5d469f4ddcb1a7
jians10 <shenxin.jiang@vanderbilt.edu>
2020-10-28 12:30:35 +0800

    StartPoint
    Generate random startPoint for player

--- ParkourNew/Assets/Scripts/PlayerScript/Manager/PlayerManger.cs
+++ ParkourNew/Assets/Scripts/PlayerScript/Manager/PlayerManger.cs
@@ -7,10 +7,17 @@ using System.IO;
 public class PlayerManger : MonoBehaviour
 {
     PhotonView PV;
-
+    public Vector3 LeftBound;
+    public Vector3 RightBound;
+    public Vector3 ChaserPosition;
+    private float Zpos;
+    private float Xpos;
     void Awake()
     {
         PV = GetComponent<PhotonView>();
+        LeftBound = GameObject.Find("LBound").transform.position;
+        RightBound = GameObject.Find("RBound").transform.position;
+        ChaserPosition = GameObject.Find("MPosition").transform.position;
     }
 
 
@@ -30,18 +37,22 @@ public class PlayerManger : MonoBehaviour
         if (PhotonNetwork.IsMasterClient)
         {
             //PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "MutantPlayer"), Vector3.zero, Quaternion.identity);
-            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerControllerNeo"), new Vector3(0, 0, 0), Quaternion.identity);
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerControllerNeo"), ChaserPosition, Quaternion.identity);
 
         }
         else
         {
-            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerControllerNeo"), new Vector3(10,0,10), Quaternion.identity);
+            RandomValueGenerator();
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerControllerNeo"), new Vector3(Xpos,LeftBound.y,Zpos), Quaternion.identity);
+            Zpos = 0;
+            Zpos = 0;
         }
     }
 
     // Update is called once per frame
-    void Update()
+    private void RandomValueGenerator()
     {
-        
+         Zpos = Random.Range(Mathf.Min(RightBound.x,LeftBound.x), Mathf.Max(RightBound.x, LeftBound.x));
+         Xpos = Random.Range(Mathf.Min(RightBound.y, LeftBound.y), Mathf.Max(RightBound.y, LeftBound.y));
     }
 }
